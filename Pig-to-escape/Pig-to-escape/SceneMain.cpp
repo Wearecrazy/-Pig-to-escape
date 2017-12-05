773f64ed319fcd9162f44732f059dfa4f256fdb5
student <student@DEV2-03>
2017-12-05 16:26:16 +0900

    11の10まで

--- Pig-to-escape/Pig-to-escape/SceneMain.cpp
+++ Pig-to-escape/Pig-to-escape/SceneMain.cpp
@@ -27,6 +27,23 @@ CSceneMain::~CSceneMain()
 //ゲームメイン初期化メソッド
 void CSceneMain::InitScene()
 {
+	//外部グラフィックファイルを読み込み0番に登録(512*512ピクセル)
+	Draw::LoadImage(L"image.png", 0, TEX_SIZE_512);
+
+	//外部グラフィックファイルを読み込み1番に登録
+	Draw::LoadImage(L"BossBack.png", 1, TEX_SIZE_512);
+
+	//主人公オブジェクト作成
+	//CObjHero* obj = new CObjHero();
+	//Objs::InsertObj(obj, OBJ_HERO, 10);
+
+	//背景オブジェクト作成
+	CObjBackground* back = new CObjBackground();
+	Objs::InsertObj(back, OBJ_BACKGROUND, 5);
+
+	//タイム初期化
+	//m_time = 0;
+
 
 
 
