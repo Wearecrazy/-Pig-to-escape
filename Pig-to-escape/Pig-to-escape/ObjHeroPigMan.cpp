8a5a3faf7d6538adc18c9f62586c30ea76cf2d48
student <student@DEV2-26>
2017-12-05 16:40:23 +0900

    主人公、豚男のオブジェクトを作った。

---
+++ Pig-to-escape/Pig-to-escape/ObjHeroPigMan.cpp
@@ -0,0 +1,26 @@
+//使用するヘッダーファイル
+#include "GameL\DrawTexture.h"
+
+#include "GameHead.h"
+#include "ObjHeroPigMan.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjHeroPigMan::Init()
+{
+	
+}
+
+//アクション
+void CObjHeroPigMan::Action()
+{
+	
+}
+
+//ドロー
+void CObjHeroPigMan::Draw()
+{
+	
+}
---
+++ Pig-to-escape/Pig-to-escape/ObjHeroPigMan.h
@@ -0,0 +1,18 @@
+#pragma once
+//使用するヘッダー
+#include "GameL\SceneObjManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：主人公,豚男
+class CObjHeroPigMan : public CObj
+{
+	public:
+		CObjHeroPigMan() {};
+		~CObjHeroPigMan() {};
+		void Init();		//イニシャライズ
+		void Action();		//アクション
+		void Draw();		//ドロー
+	private:
+	
+}; 
