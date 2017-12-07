e23d02644f50e61959682cf30353e304cfb5061a
kcomdev <kcomdev@gmail.com>
2017-12-07 12:18:52 +0900

    stageのソース打ち始めた

---
+++ Pig-to-escape/Pig-to-escape/ObjBlock.cpp
@@ -0,0 +1,10 @@
+#include"GameL\DrawTexture.h"
+#include"GameL\WinInputs.h"
+#include"GameL\SceneManager.h"
+#include"GameL\SceneObjManager.h"
+#include"GameHead.h"
+#include"ObjBlock.h"
+
+//使用するネームスペース
+using namespace GameL;
+
---
+++ Pig-to-escape/Pig-to-escape/ObjBlock.h
@@ -0,0 +1,20 @@
+#pragma once
+//使用するヘッダー
+#include"GameL\SceneObjManager.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト:ブロック＆背景
+class CObjBlock :public CObj
+{
+public:
+	CObjBlock(int map[150][20]);
+	~CObjBlock() {};
+	void Init();//イニシャライズ
+	void Action();//アクション
+	void Draw();//ドロー
+private:
+
+
+};
